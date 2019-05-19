0e970402283d062009e3c6b167093a4b4544e5cb
yomoda07 <kanji.yy@gmail.com>
2019-05-19 11:29:09 +0900

    Add s3 and elastic ip configuration

--- aws.tf
+++ aws.tf
@@ -5,8 +5,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+# S3 bucket
+resource "aws_s3_bucket" "s3_bucket" {
+  # S3 bucket names must be unique across all AWS accounts
+  bucket = "k-yomo-terraform-practice"
+  acl    = "private"
+}
+
+// EC2 instance
+resource "aws_instance" "instance" {
   // Ubuntu 16.10 AMI
   ami = "ami-b374d5a5"
   instance_type = "t2.micro"
+
+  // explicit dependency
+  depends_on = ["aws_s3_bucket.s3_bucket"]
+}
+
+// Elastic IP
+resource "aws_eip" "ip" {
+  // implicit dependency that requires terraform to create instance first, then ip
+  instance = "${aws_instance.instance.id}"
 }
